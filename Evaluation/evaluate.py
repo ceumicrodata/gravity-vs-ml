987a3b0f96959c8b479c56695c3b4f839784a539
rgyuri <ruzicska.gyorgy@gmail.com>
2023-04-08 12:04:57 +0200

    Added Common Part of Commuters metric to evaluator

--- Evaluation/evaluate.py
+++ Evaluation/evaluate.py
@@ -27,12 +27,16 @@ def r2(predictions: pd.Series, targets: pd.Series) -> float:
     ssr = np.sum((targets-predictions)**2)
     return 1-ssr/sst
 
+def cpc(predictions: pd.Series, targets: pd.Series) -> float:
+    numerator = 2.0 * np.sum(np.minimum(predictions, targets)) 
+    denominator = np.sum(predictions) + np.sum(targets)
+    return numerator/denominator
 
 project_home = pathlib.Path("../")
 result_files = list(str(x) for x in project_home.rglob('*') if 'prediction' in str(x).split("/")[-1] and '.csv' in str(x))
 print(f"Found {len(result_files)} model result file(s):\n",*[f"{x}\n" for x in result_files])
 
-measure_names = ["MAE", "RMAE", "MSE", "RMSE", "PSEUDOR2"]
+measure_names = ["MAE", "RMAE", "MSE", "RMSE", "PSEUDOR2", "CommonPartOfCommuters"]
 repo = git.Repo(search_parent_directories=True)
 sha = repo.head.object.hexsha
 
@@ -40,7 +44,7 @@ for result_file in result_files:
     try:
         predictions = pd.read_csv(result_file).prediction
         targets = pd.read_csv(result_file).target
-        measures = dict(zip(measure_names,(mae(predictions,targets), rmae(predictions,targets), mse(predictions, targets), rmse(predictions, targets), r2(predictions, targets))))
+        measures = dict(zip(measure_names,(mae(predictions,targets), rmae(predictions,targets), mse(predictions, targets), rmse(predictions, targets), r2(predictions, targets), cpc(predictions,targets))))
         measures['sha'] = sha
         measures['path'] = result_file
     except AttributeError:
